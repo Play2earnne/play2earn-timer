7d6465b87b99f5d885b4348ac4e33dec31dc8057
anand <vermaanand278@gmail.com>
2024-09-21 20:50:07 +0530

    dsf

--- index.js
+++ index.js
@@ -60,7 +60,6 @@ if (x) {
     x = false;
   }, secondsUntilNextMinute * 1000);
 }
-///////////////////
 // mailSender("vermaanand278@gmail.com", "THis is simple tile", otpTemplate(9918))
 httpServer.listen(PORT, () => {
   console.log("Server listening on port", PORT);
